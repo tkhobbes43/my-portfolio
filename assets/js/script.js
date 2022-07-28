62f7909e4d67fea7fd2899dc85a4f2ccd2cccd64
Tommy Lee <t.k.hobbes43@gmail.com>
2022-07-28 18:14:11 -0500

    using Bulma columns for projects section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
 function openTab (event, tabId) {
-
-//  Get all elements with the class tabcontent and hide them
+ //  Get all elements with the class tabcontent and hide them
     tabcontent = document.getElementsByClassName('tabcontent');
     for ( i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = 'none';        
     }
-// Get all elements with the class tablinks and remove the class is-active
+ // Get all elements with the class tablinks and remove the class is-active
     tablinks = document.getElementsByClassName('tablinks');
     for ( i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace('is-active', '');        
     }
-// Show current tab and add is-active to that tab
+ // Show current tab and add is-active to that tab
     document.getElementById(tabId).style.display = 'block';
     event.currentTarget.className += " is-active";
 }
