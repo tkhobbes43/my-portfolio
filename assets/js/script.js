871de2081ebfdd7c8fe97f211b2c03f387d88839
Tommy Lee <t.k.hobbes43@gmail.com>
2022-07-28 15:11:55 -0500

    javascript for tab not working quite right

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function openTab (event, tabId) {
+
+//  Get all elements with the class tabcontent and hide them
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for ( i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';        
+    }
+// Get all elements with the class tablinks and remove the class is-active
+    tablinks = document.getElementsByClassName('tablinks');
+    for ( i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace('is-active', '');        
+    }
+// Show current tab and add is-active to that tab
+    document.getElementById(tabId).style.display = 'block';
+    event.currentTarget.className += " is-active";
+}
--- index.js
+++
@@ -1,6 +0,0 @@
-
-//  Get all elements with the class tabcontent and hide them
-
-// Get all elements with the class tablinks and remove the class is-active
-
-// Show current tab and add is-active to that tab
