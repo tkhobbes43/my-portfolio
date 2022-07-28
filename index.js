8103115a99069aff40135e103ab8ac91ace4bbe0
Tommy Lee <t.k.hobbes43@gmail.com>
2022-07-28 14:37:49 -0500

    javascript needed for tabs

---
+++ index.js
@@ -0,0 +1,6 @@
+
+//  Get all elements with the class tabcontent and hide them
+
+// Get all elements with the class tablinks and remove the class is-active
+
+// Show current tab and add is-active to that tab
